8dcf1392c0c2eab755bbdf342c4342dcbbda069d
Everton Rocha <imevertonrch@gmail.com>
2025-04-11 23:06:55 -0300

    feat: implementando validação dos campos

--- src/app/usuario/registrar-usuario.ts
+++ src/app/usuario/registrar-usuario.ts
@@ -3,27 +3,44 @@ import RegistrarUsuario from "@/core/usuario/service/registrar-usuario";
 import RepositorioUsuarioPg from "@/adapter/database/repositorio-usuario-pg";
 import TerminalUtil from "../util/terminal-util";
 import Usuario from "@/core/usuario/model/Usuario";
+import ValidatorUtils from "../util/validator-util";
+
+type DadosInput = {
+    nome: string
+    email: string
+    senha: string
+}
 
 export default async function registrarUsuario() {
     TerminalUtil.titulo("Registrar Usuário")
     
-    const nome = await TerminalUtil.campoRequerido("Nome:")
-    const email = await TerminalUtil.campoRequerido("Email:")
-    const senha = await TerminalUtil.campoRequerido("Senha:")
-
-    const usuario: Usuario = {nome, email, senha}
-    
     try {
+        const {nome, email, senha} = await capturaDados()
+        
+        const usuario: Usuario = {nome, email, senha}
+        
         const repositorio = new RepositorioUsuarioPg()
         const senhaCripto = new BcryptSenhaCripto()
         const casoDeUso = new RegistrarUsuario(repositorio, senhaCripto)
-
+        
         await casoDeUso.executar(usuario)
-
+        
         TerminalUtil.sucesso(`Usuário ${usuario.nome} registrado com sucesso.`)
     } catch(e: any) {
         TerminalUtil.erro(e.message)
     } finally {
         await TerminalUtil.esperarEnter()
+    }
+
+    async function capturaDados(): Promise<DadosInput> {
+        const nome = await TerminalUtil.campoRequerido("Nome:")
+    
+        const email = await TerminalUtil.campoRequerido("Email (user@mail.com):")
+        ValidatorUtils.ehEmailValido(email)
+    
+        const senha = await TerminalUtil.campoRequerido("Senha (minímo 8, maiúsculas, números e caracteres especiais) :")
+        ValidatorUtils.ehSenhaForte(senha)
+    
+        return {nome, email, senha}
     }
 }
--- src/core/shared/Erros.ts
+++ src/core/shared/Erros.ts
@@ -1,5 +1,7 @@
 const Erros = {
-    USUARIO_JA_EXISTE: "USUARIO_JA_EXISTE"
+    USUARIO_JA_EXISTE: "USUARIO_JA_EXISTE",
+    EMAIL_MAL_FORMADO: "EMAIL_MAL_FORMADO",
+    SENHA_FRACA: "SENHA_FRACA"
 } as const
 
 export default Erros
