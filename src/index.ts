f446ca8105f705366983a1a853d31f06510cbada
Everton Rocha <imevertonrch@gmail.com>
2025-04-12 21:05:15 -0300

    feat: definindo rotas da api

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,36 @@ import dotenv from "dotenv"
 dotenv.config()
 
 import express from "express"
+import RegistrarUsuarioController from "./external/api/registrar-usuario-controller"
+import RegistrarUsuario from "./core/usuario/service/registrar-usuario"
+import RepositorioUsuarioPg from "./external/database/repositorio-usuario-pg"
+import BcryptSenhaCripto from "./external/security/bcrypt-senha-cripto"
+import LoginUsuario from "./core/usuario/service/login-usuario"
+import LoginUsuarioController from "./external/api/login-usuario-controller"
+import ValidatorImpl from "./external/validator/validator-impl"
+import ObterProdutoPorId from "./core/produto/service/obter-produto-por-id"
+import ObterProdutoPorIdController from "./external/api/obter-produto-por-id-controller"
+import UsuarioMiddleware from "./external/api/usuario-middleware"
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+const repositorioUsuario = new RepositorioUsuarioPg()
+const provedorCripto = new BcryptSenhaCripto()
+const validadorDados = new ValidatorImpl()
+
+// casos de uso
+const registrarUsuario = new RegistrarUsuario(repositorioUsuario, provedorCripto) 
+const loginUsuario = new LoginUsuario(repositorioUsuario, provedorCripto, validadorDados)
+
+new RegistrarUsuarioController(app, registrarUsuario)
+new LoginUsuarioController(app, loginUsuario)
+
+// ----- Rota protegida
+const usuarioMiddleware = UsuarioMiddleware(repositorioUsuario)
+const obterProdutoPorId = new ObterProdutoPorId()
+
+new ObterProdutoPorIdController(app, obterProdutoPorId, usuarioMiddleware)
 
 app.listen(process.env.API_PORT ?? 3000)
