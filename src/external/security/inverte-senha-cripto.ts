6c85aaf8277440babee895e3e421d6b08e4584ba
Everton Rocha <imevertonrch@gmail.com>
2025-04-12 21:07:47 -0300

    feat: implementando comparação de senhas

--- src/external/security/inverte-senha-cripto.ts
+++ src/external/security/inverte-senha-cripto.ts
@@ -1,8 +1,12 @@
 import ProvedorCriptografia from "../../core/usuario/service/provedor-criptografia";
 
 export default class InverteSenhaCripto implements ProvedorCriptografia {
-
+    
     criptografar(texto: string): string {
         return texto.split("").reverse().join("")
+    }
+    
+    comparar(senha: string, encriptada: string): boolean {
+        return this.criptografar(senha) === encriptada
     }
 }
