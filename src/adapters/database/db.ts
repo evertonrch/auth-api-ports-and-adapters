3265ccb14d3d2a18cea3ca70a24b72f35173d392
Everton Rocha <imevertonrch@gmail.com>
2025-04-14 20:44:39 -0300

    chore: renomeando variáveis de ambiente

--- src/adapters/database/db.ts
+++ src/adapters/database/db.ts
@@ -6,8 +6,8 @@ const db = pgp({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWD,
-    database: process.env.DB_DATABASE
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 export default db
